73e6f3ac6ca72e79d89e3ae5313049d2ffb5a5b5
Hind <hind.nayhi@gmail.com>
2022-08-02 09:13:29 -0600

    Check eslint and fixed the errors.

--- typewriter.js
+++ typewriter.js
@@ -3,14 +3,14 @@ let i = 0; //Start index.
 
 const printEachWord = function() {
   setTimeout(() => {
-      process.stdout.write(sentence[i++]); //Increment each time by 1.
+    process.stdout.write(sentence[i++]); //Increment each time by 1.
 
-      if (i < sentence.length) { //Keep calling printEachWord() as long as the condition is true.
-        printEachWord();
-      }
-      if (i === sentence.length) { //When it reaches the last element.
-        process.stdout.write('\n');
-      }
-  }, 50)
-}
+    if (i < sentence.length) { //Keep calling printEachWord() as long as the condition is true.
+      printEachWord();
+    }
+    if (i === sentence.length) { //When it reaches the last element.
+      process.stdout.write('\n');
+    }
+  }, 50);
+};
 printEachWord();
