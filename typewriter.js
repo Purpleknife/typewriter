1b75cb2ccc50dc7bec40671166de24d8b36a9a66
Hind <hind.nayhi@gmail.com>
2022-08-02 13:01:36 -0600

    Refactored setTimout function after attending the lecture.

--- typewriter.js
+++ typewriter.js
@@ -1,16 +1,16 @@
 const sentence = "hello there from lighthouse labs";
-let i = 0; //Start index.
+let count = 0; //Start counter.
 
-const printEachWord = function() {
+const printEachLetter = function() {
   setTimeout(() => {
-    process.stdout.write(sentence[i++]); //Increment each time by 1.
+    process.stdout.write(sentence[count++]); //Increment each time by 1.
 
-    if (i < sentence.length) { //Keep calling printEachWord() as long as the condition is true.
-      printEachWord();
+    if (count < sentence.length) { //Keep calling printEachLetter() as long as the condition is true.
+      printEachLetter();
     }
-    if (i === sentence.length) { //When it reaches the last element.
+    if (count === sentence.length) { //When it reaches the last element.
       process.stdout.write('\n');
     }
   }, 50);
 };
-printEachWord();
+printEachLetter();
